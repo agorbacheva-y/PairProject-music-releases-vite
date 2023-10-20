46e1e8e2924869e4742cb7b4d8168f71f7c2867f
agorbacheva-y <agorbacheva.y@gmail.com>
2023-10-20 23:37:54 +0200

    logic for multiple artist names

--- src/components/ArtistName.jsx
+++ src/components/ArtistName.jsx
@@ -1,20 +1,38 @@
 import PropTypes from 'prop-types';
 
 const ArtistName = ({ artistName, externalURL  }) => {
-  console.log(artistName);
+  //console.log(artistName);
+
+  // Function to add '&' btwn artists if there are only 2 artists
+  // if there are >2 artists, show ',' between all artists 
+  // except the last two which should have '&':
+  const addCharacter = (index) => {
+    let character;
+    if (artistName.length === 2 && index < artistName.length - 1) {
+      character = " & ";
+    } else if (artistName.length > 2 && index < artistName.length - 2) {
+      character = ", ";
+    } else if (artistName.length > 2 && index === artistName.length - 2) {
+      character = " & ";
+    } else {
+      character = "";
+    }
+
+    return character;
+  }
 
   return (
     <div className="artist-name">
       {artistName.map(( artists, index ) => (
         <a
-        style={{ textDecoration: 'none', color: 'white' }}
+        style={{ textDecoration: 'none', color: '#a0a0a0' }}
         key={artists.id}
         href={externalURL}
         target="_blank"
         rel="noopener noreferrer"
       >
           {artists.name}
-          {index < artistName.length -1 ? " & " : "" }
+          {addCharacter(index)}
         </a>
       ))}
     </div>
@@ -27,3 +45,6 @@ ArtistName.propTypes = {
 };
 
 export default ArtistName;
+
+// Initial code to add '&' btwn artist names:
+// index < artistName.length -1 ? " & " : "" 
